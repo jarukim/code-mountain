969587f9da6820bf93c0810f46a3a744bd037afc
jarukim <baer2000@naver.com>
2022-08-10 00:09:56 +0900

    main 초기화

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,6 @@ import baek.Baek1010;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        Baek1010 baek1010 = new Baek1010();
-        baek1010.solution();
+
     }
 }
