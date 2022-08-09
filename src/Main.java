7b61ea831cae724200275898b47edcdc8c028432
jarukim <baer2000@naver.com>
2022-08-10 00:43:10 +0900

    문제풀이

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
+import baek.Baek1271;
+import baek.Baek2338;
+import baek.Baek3003;
+
 public class Main {
     public static void main(String[] args) {
-
+        Baek3003 baek3003 = new Baek3003();
+        baek3003.solution();
     }
 }
